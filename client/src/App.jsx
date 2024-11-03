77b8d4e1ac41e87ebce2b54efaf6a38f75cd6f63
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-03 21:13:17 +0100

    test i18n

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,38 @@
+import { useTranslation } from "react-i18next";
+import i18n from "./utils/i18n";
+
+const languages = [
+    {
+        code: "en",
+        name: "English",
+    },
+    {
+        code: "fr",
+        name: "Français",
+    },
+    {
+        code: "ar",
+        name: "العربية",
+    },
+];
 function App() {
-    return <h1>Hello World!</h1>;
+    const { t } = useTranslation();
+    return (
+        <div>
+            <h1>{t("welcomeReact")}</h1>
+            <ul>
+                {languages.map((language) => (
+                    <li key={language.code}>
+                        <button
+                            onClick={() => i18n.changeLanguage(language.code)}
+                        >
+                            {language.name}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
