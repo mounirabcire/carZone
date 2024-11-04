59a62119bcf0ba5670282bdc09751cbefb1f110b
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-04 12:11:54 +0100

    set up the globales style

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import "./utils/i18n.js";
 import App from "./App.jsx";
+import GlobalStyles from "./styles/GlobalStyles.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <GlobalStyles />
         <App />
     </StrictMode>
 );
