5d4dc740de6e38194599c01bf7498285a17b6d8d
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-11 09:01:58 +0100

    create data folder and add partners data

---
+++ client/src/data/data.js
@@ -0,0 +1,42 @@
+export const partners = [
+    {
+        fullName: "Daniel Foster",
+        profession: "Founder of a premium car rental agency",
+        img: "/assets/images/partner__img.jpg",
+    },
+    {
+        fullName: "Robert Mitchell",
+        profession: "Auto accessories business innovator",
+        img: "/assets/images/partner__img-2.jpg",
+    },
+    {
+        fullName: "James Cooper",
+        profession: "Expert in luxury car rentals",
+        img: "/assets/images/partner__img-3.jpg",
+    },
+    {
+        fullName: "Alex Carter",
+        profession: "Owner of a popular auto accessories brand",
+        img: "/assets/images/partner__img-4.jpg",
+    },
+    {
+        fullName: "Sophia Morgan",
+        profession: "CEO of an electric car rental startup",
+        img: "/assets/images/partner__img-5.jpg",
+    },
+    {
+        fullName: "Lucas Barnes",
+        profession: "Automotive marketing consultant",
+        img: "/assets/images/partner__img-6.jpg",
+    },
+    {
+        fullName: "Evelyn Brooks",
+        profession: "Specialist in performance car modifications",
+        img: "/assets/images/partner__img-7.jpg",
+    },
+    {
+        fullName: "Liam Rodriguez",
+        profession: "Founder of eco-friendly car accessories line",
+        img: "/assets/images/partner__img-8.jpg",
+    },
+];
