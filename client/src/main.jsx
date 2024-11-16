a142b16dd88a4714fc579ce5e3ea45aa2b2cda53
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-16 09:32:31 +0100

    set up remixicon and react-toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import 'remixicon/fonts/remixicon.css'
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, Zoom } from "react-toastify";
+import "remixicon/fonts/remixicon.css";
 import "./utils/i18n.js";
 import App from "./App.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
@@ -10,5 +12,11 @@ createRoot(document.getElementById("root")).render(
     <StrictMode>
         <GlobalStyles />
         <App />
+        <ToastContainer
+            position="bottom-right"
+            pauseOnFocusLoss={false}
+            autoClose={3000}
+            transition={Zoom}
+        />
     </StrictMode>
 );
