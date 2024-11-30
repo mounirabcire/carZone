563995e6e83874d4f07377ed2cb33e299bfd5146
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-11-30 09:01:21 +0100

    rename the exported variable and refactor the images path

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,42 +1,44 @@
-export const partners = [
+// NOTE: DATA FOR TESTING ONLY;
+
+export const partnersData = [
     {
         fullName: "Daniel Foster",
-        profession: "Founder of a premium car rental agency",
-        img: "/assets/images/partner__img.jpg",
+        profession: "Founder of a premium car sellal agency",
+        img: "/assets/images/partners/partner__img.jpg",
     },
     {
         fullName: "Robert Mitchell",
         profession: "Auto accessories business innovator",
-        img: "/assets/images/partner__img-2.jpg",
+        img: "/assets/images/partners/partner__img-2.jpg",
     },
     {
         fullName: "James Cooper",
-        profession: "Expert in luxury car rentals",
-        img: "/assets/images/partner__img-3.jpg",
+        profession: "Expert in luxury car sellals",
+        img: "/assets/images/partners/partner__img-3.jpg",
     },
     {
         fullName: "Alex Carter",
         profession: "Owner of a popular auto accessories brand",
-        img: "/assets/images/partner__img-4.jpg",
+        img: "/assets/images/partners/partner__img-4.jpg",
     },
     {
         fullName: "Sophia Morgan",
-        profession: "CEO of an electric car rental startup",
-        img: "/assets/images/partner__img-5.jpg",
+        profession: "CEO of an electric car sellal startup",
+        img: "/assets/images/partners/partner__img-5.jpg",
     },
     {
         fullName: "Lucas Barnes",
         profession: "Automotive marketing consultant",
-        img: "/assets/images/partner__img-6.jpg",
+        img: "/assets/images/partners/partner__img-6.jpg",
     },
     {
         fullName: "Evelyn Brooks",
         profession: "Specialist in performance car modifications",
-        img: "/assets/images/partner__img-7.jpg",
+        img: "/assets/images/partners/partner__img-7.jpg",
     },
     {
         fullName: "Liam Rodriguez",
         profession: "Founder of eco-friendly car accessories line",
-        img: "/assets/images/partner__img-8.jpg",
+        img: "/assets/images/partners/partner__img-8.jpg",
     },
 ];
