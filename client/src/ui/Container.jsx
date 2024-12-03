dc5be407fa11d1564f40f79ebdd32f173c95e586
Mounir Abcire <mounirwebdevcode@gmail.com>
2024-12-03 21:45:23 +0100

    create a container componenet

---
+++ client/src/ui/Container.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 2rem;
+
+    @media (min-width: 40em) {
+        max-width: 640px;
+    }
+
+    @media (min-width: 48em) {
+        max-width: 768px;
+    }
+
+    @media (min-width: 64em) {
+        max-width: 1024px;
+    }
+
+    @media (min-width: 80em) {
+        max-width: 1280px;
+    }
+
+    @media (min-width: 96em) {
+        max-width: 1536px;
+    }
+`;
+
+export default Container;
